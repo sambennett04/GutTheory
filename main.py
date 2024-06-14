d9c7dab764523d8342c5cf27adf554535b11abef
srbennett01 <sambennett@Sams-Air.fios-router.home>
2024-06-14 12:03:24 -0400

    Added 2 endpoints, one with functionality to check if a food starts with c and the other to make a distinct count of a list

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 from typing import Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel #help with typing for API's, helping you define a data type held within your API
 
 
-class Vegetables(BaseModel):
-    listOfVeges: list[str]
+class Vegetables(BaseModel): #definition of shape of request and manner to validate requests made to the API
+    listOfFoods: list[str]
+
+class Foods(BaseModel):
+    listOfFoods: list[str]
 
 
 app = FastAPI()
 
 
-@app.post("/vegetables/")
-async def create_item(vegetables: Vegetables):
-    return vegetables
+@app.post("/vegetables/") #change to classifyFood
+async def classify_Food(vegetables: Vegetables):
+    result = []
+    for v in vegetables.listOfFoods:
+        if (v[0].lower() == 'c'):
+            result.append(v)
+    return {"foodsBeginWithTheLetterC": result} #key that describes the data that is being returned from the server
+
+@app.post("/countDistinct")
+async def count_Distinct(foods: Foods):
+    mySet = set(foods.listOfFoods) 
+    gte30 = len(mySet) >= 30
+    return {"distinctCount":len(mySet), "overUnder30":gte30}

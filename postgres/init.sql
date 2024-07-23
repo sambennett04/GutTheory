4b2838c9343b85069233079bd561d3183307e108
srbennett01 <sambennett@Sams-Air.fios-router.home>
2024-07-23 09:32:15 -0400

    Created and tested new tabledbo. plant_foods added csv file to postgres folder with all plants and vegetables from previous txt files. Will add more plant foods later.

--- postgres/init.sql
+++ postgres/init.sql
@@ -10,6 +10,16 @@ CREATE TABLE dbo.user (
   UNIQUE (email)
 );
 
+-- The type serial stores a sequential integer (INT) that is automatically assigned by the database server when a new row is intserted
+-- The type varchar(256) is a char always stored at 256 characters 
+
+CREATE TABLE dbo.plant_foods (
+  plant_id serial PRIMARY KEY, 
+  food_name varchar(256) NOT NULL UNIQUE,
+  food_kind varchar(256) NOT NULL,
+  UNIQUE (food_name)
+)
+
 -- TO DO: Create a table to represent data for the food classifier
 -- TO DO: Add sample data to the users table
 -- TO DO: Automatically add all data from the food classifier csvs to postgres on startup
